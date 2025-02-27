5daa5ca4b4eedc03af41208023b6392bba23ad80
Omer Serruya <omerts58@gmail.com>
2025-02-27 20:44:38 +0000

    Added 3rd party auto (google & git) and add cookies -almost full implement

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import initApp from "./server";
-const port = process.env.PORT;
-
-initApp().then((app) => {
-  app.listen(port, () => {
-    // console.log(`[ ${new Date().toISOString()} ] Server is running on port ${port}`);
-  });
-});
